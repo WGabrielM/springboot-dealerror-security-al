df35418d0fcc7cac91d9e33526022cff87316ce6
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-21 09:34:47 -0300

    add error stacktrace on properties

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -54,4 +54,11 @@ public class DoctorController {
 
         return ResponseEntity.noContent().build();
     }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalalhamentoMedico(medico));
+    }
 }
