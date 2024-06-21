0adfcb108b5926abab2e0fa1d41b6d42d0bbd7a8
Wellington <wellingtongmb@gmail.com>
2024-06-21 19:09:38 -0300

    fix securityfilter,implements token for all request except for login  and test conections with token

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,16 +11,34 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
+        http
+                // Desabilitar CSRF
+                .csrf(csrf -> csrf.disable())
+
+                // Configurar a sessão para ser Stateless
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+
+                // Configurar autorização das requisições
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/login").permitAll(); // Permitir acesso irrestrito ao endpoint /login
+                    req.anyRequest().authenticated(); // Requer autenticação para qualquer outra requisição
+                })
+
+                // Adicionar filtro de autenticação JWT antes do filtro padrão de autenticação de usuário e senha
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build(); // Construir e retornar o SecurityFilterChain uma única vez
     }
 
     @Bean
@@ -31,5 +50,4 @@ public class SecurityConfigurations {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
